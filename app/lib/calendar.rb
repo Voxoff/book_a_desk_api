0de84614b70e5e758b3b857e7fe61875f1dbbea4
Guy Bennett-Jones <Voxoff>
2019-04-26 13:06:11 +0100

    dev check|

--- app/lib/calendar.rb
+++ app/lib/calendar.rb
@@ -35,7 +35,7 @@ module Calendar
       authorizer = Google::Auth::UserAuthorizer.new(client_id, @scope, token_store)
       user_id = 'default'
       credentials = authorizer.get_credentials(user_id)
-      if credentials.nil?
+      if Rails.env.development? && credentials.nil?
         url = authorizer.get_authorization_url(base_url: @oob_uri)
         puts 'Open the following URL in the browser and enter the ' \
             "resulting code after authorization:\n" + url
